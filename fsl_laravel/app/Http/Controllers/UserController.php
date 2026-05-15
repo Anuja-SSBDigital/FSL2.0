fa9ce543663838ef5ca4580580481cb1846552b5
Meet Vaghela <meet.vaghela@ssbi.in>
2026-05-15 10:27:01 +0530

    Update UserController.php

--- fsl_laravel/app/Http/Controllers/UserController.php
+++ fsl_laravel/app/Http/Controllers/UserController.php
@@ -4,20 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Services\FlureeService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
    
 public function userpage(FlureeService $fluree)
 {
+   // Get current authenticated user
+   $currentUser = Session::get('fluree_user');
+   if (!$currentUser) {
+       return redirect()->route('login');
+   }
+
    $query = [
     "select" => [
         "?u" => [
+            "_id",
+            "userdetails/_id",
             "firstname",
             "lastname",
             "email",
             "username",
-            "appointmentletter"
+            "mobileno",
+            "designation",
+            "isactive",
+            "appointmentletter",
+            "role_id",
+            "dept_code",
+            "inst_id"
         ]
     ],
     "where" => [
@@ -31,7 +46,7 @@ public function userpage(FlureeService $fluree)
 
     $users = $fluree->query($query);
  
-    return view('users.userpage', compact('users'));
+    return view('users.userpage', compact('users', 'currentUser'));
 }
 
 }
